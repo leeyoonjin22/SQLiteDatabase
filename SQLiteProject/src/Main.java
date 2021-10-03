12432e5f4049f4aa600b3bd3a328722c205117fc
leeyoonjin22 <leeyi0190@naver.com>
2021-10-03 16:20:52 +0900

    crud구

--- SQLiteProject/src/Main.java
+++ SQLiteProject/src/Main.java
@@ -14,18 +14,60 @@ public class Main {
 		String dbFile = "myfirst.db";
 		con = DriverManager.getConnection("jdbc:sqlite:"+dbFile);
 		
-		//데이터조회
-		System.out.println("\n***데이터 조회 ***");
-		Statement stat1 = con.createStatement();
-		String sql1 = "select * from g_artists";
-		ResultSet rs1 = stat1.executeQuery(sql1);
-		while(rs1.next()) {
-			String id = rs1.getString("id");
-			String name = rs1.getString("name");
-			System.out.println(id+" "+ name);
+	
+		
+//	// 데이터 추가
+//		System.out.println("\n*** 새 데이터 추가 ***");
+//		Statement stat2 = con.createStatement();
+//		String sql2 = "insert into g_artists (name, a_type, a_year, debut, regdate)"
+//				+ " values ('방탄소년단', '남성', '2010년대', '2013년',datetime('now', 'localtime'));";
+//		int cnt = stat2.executeUpdate(sql2);
+//		if(cnt >0)
+//			System.out.println("새로운 데이터가 추가되었습니다!");
+//		else
+//			System.out.println("[Error] 데이터 추가 오류!");
+//	stat2.close();
+	
+	
+	// 데이터 수정
+	System.out.println("\n *** 데이터 수정 ***");
+	Statement stat3 = con.createStatement();
+	String sql3 = "update g_artists set name = '소녀시대'"
+			+ "where id=6;";
+	int cnt3 = stat3.executeUpdate(sql3);
+	if(cnt3>0)
+		System.out.println("데이터가 수정되었습니다!");
+	else 
+		System.out.println("[Error] 데이터 수정 오류!");
+	stat3.close();
+	
+	
+		
+		// 데이터 삭제
+		System.out.println("\n *** 데이터 삭제 ***");
+	Statement stat4 = con.createStatement();
+	String sql4 = "delete from g_artists where id=7;";
+			int cnt4 = stat4.executeUpdate(sql4);
 			
-		}
-		stat1.close();
+		if(cnt4>0)
+			System.out.println("데이터가 삭제되었습니다!");
+		else
+			System.out.println("[Error] 데이터 삭제 오류!");
+	
+		stat4.close();
+		
+		//데이터조회
+				System.out.println("\n***데이터 조회 ***");
+				Statement stat1 = con.createStatement();
+				String sql1 = "select * from g_artists";
+				ResultSet rs1 = stat1.executeQuery(sql1);
+				while(rs1.next()) {
+					String id = rs1.getString("id");
+					String name = rs1.getString("name");
+					System.out.println(id+" "+ name);
+					
+				}
+				stat1.close();
 		
 	}catch(Exception e) {
 		e.printStackTrace();
